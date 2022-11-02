82b6b33bf251b268ab39eaee8e6325bf67a536b0
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-11-02 16:29:43 +0700

    Update

--- DAO/DAO1.py
+++ DAO/DAO1.py
@@ -3,17 +3,17 @@ import pymysql
 
 def connectDB():
     # Tân
-    host = 'localhost'
-    dbName = 'TextSummary'
-    userName = 'root'
-    password = 'tan20011234'
-    conn = pymysql.connect(host=host, user=userName, password=password, database=dbName)
-    return conn
-
-    # # Dương
     # host = 'localhost'
-    # dbName = 'textsummarydb'
+    # dbName = 'TextSummary'
     # userName = 'root'
-    # password = '123456789'
+    # password = 'tan20011234'
     # conn = pymysql.connect(host=host, user=userName, password=password, database=dbName)
     # return conn
+
+    # # Dương
+    host = 'localhost'
+    dbName = 'textsummarydb'
+    userName = 'root'
+    password = '123456789'
+    conn = pymysql.connect(host=host, user=userName, password=password, database=dbName)
+    return conn
